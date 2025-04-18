9abf9a7591b2d857bcb60dc83736e764c75f90f5
Doyeon Kim <nsa10050@naver.com>
2025-04-18 10:25:22 +0900

    vercel 배포 설정 완료

--- api/kcisa.js
+++ api/kcisa.js
@@ -7,21 +7,7 @@ const BASE_URL = 'http://api.kcisa.kr/openapi/API_CCA_148/request';
 const SERVICE_KEY = '356b0d91-82e9-43e0-b690-b78a982ec774';
 
 router.get('/', async (req, res) => {
-  const { pageNo = 1, numOfRows = 20 } = req.query;
-  const response = await axios.get(BASE_URL, {
-    params: {
-      serviceKey: SERVICE_KEY,
-      numOfRows,
-      pageNo,
-      format: 'json'
-    },
-    headers: {
-      'User-Agent': 'Mozilla/5.0',
-      'Accept': 'application/json',
-    }
-  });
-
-  res.json(response.data);
+  res.send('sdsdfsdfs')
 });
 
 module.exports = router;
